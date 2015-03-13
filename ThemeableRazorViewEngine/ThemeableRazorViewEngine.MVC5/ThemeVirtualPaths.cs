dcff8c37315feae926ac860d7f1e88ff245e848d
Erich <qbantek@gmail.com>
2015-03-13 01:51:14 -0400

    Virtual paths for theme's content files.

---
+++ ThemeableRazorViewEngine/ThemeableRazorViewEngine.MVC5/ThemeVirtualPaths.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Quintsys.Web.ThemeableRazorViewEngine.Annotations;
+
+namespace Quintsys.Web.ThemeableRazorViewEngine
+{
+    public static class ThemeVirtualPaths
+    {
+        /// <summary>
+        /// Virtual path for a theme's image. Default theme = 'Default'.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <param name="themeName">Name of the theme.</param>
+        /// <returns></returns>
+        public static string Image(string filename, string themeName)
+        {
+            return VirtualPath(ContentLocationFormats.Images, filename, themeName);
+        }
+
+        /// <summary>
+        /// Virtual path for a theme's stylesheet. Default theme = 'Default'.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <param name="themeName">Name of the theme.</param>
+        /// <returns></returns>
+        public static string Stylesheet(string filename, string themeName)
+        {
+            return VirtualPath(ContentLocationFormats.Stylesheets, filename, themeName);
+        }
+
+        private static string VirtualPath([NotNull] string pathTemplate,
+            [NotNull] string filename,
+            string themeName = "Default")
+        {
+            if (pathTemplate == null)
+                throw new ArgumentNullException("pathTemplate");
+            if (filename == null)
+                throw new ArgumentNullException("filename");
+
+            return string.Format(pathTemplate, themeName, filename);
+        }
+
+        private static class ContentLocationFormats
+        {
+            public const string Images = "~/Themes/{0}/Content/images/{1}";
+            public const string Stylesheets = "~/Themes/{0}/Content/css/{1}";
+        }
+    }
+}
--- ThemeableRazorViewEngine/ThemeableRazorViewEngine.MVC5/UrlHelperExtensions.cs
+++ ThemeableRazorViewEngine/ThemeableRazorViewEngine.MVC5/UrlHelperExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Quintsys.Web.ThemeableRazorViewEngine.Annotations;
 
 namespace Quintsys.Web.ThemeableRazorViewEngine
@@ -7,10 +6,10 @@ namespace Quintsys.Web.ThemeableRazorViewEngine
     public static class UrlHelperExtensions
     {
         /// <summary>
-        /// Application absolute path for an image file, taking themes into  consideration. Default theme name = 'Default'.
+        /// Application absolute path for a theme's image file.
         /// </summary>
         /// <param name="urlHelper">The URL helper.</param>
-        /// <param name="filename">The filename.</param>
+        /// <param name="filename">The filename for the image.</param>
         /// <returns></returns>
         [UsedImplicitly]
         public static string ThemeImageUrl(this UrlHelper urlHelper, string filename)
@@ -19,23 +18,24 @@ namespace Quintsys.Web.ThemeableRazorViewEngine
         }
 
         /// <summary>
-        /// Application absolute path for an image file, taking themes into  consideration.
+        /// Application absolute path for a theme's image file.
         /// </summary>
         /// <param name="urlHelper">The URL helper.</param>
-        /// <param name="filename">The filename.</param>
+        /// <param name="filename">The filename for the image.</param>
         /// <param name="themeName">Name of the theme.</param>
         /// <returns></returns>
         [UsedImplicitly]
         public static string ThemeImageUrl(this UrlHelper urlHelper, string filename, string themeName)
         {
-            return ContentUrl(urlHelper, ContentLocationFormats.ImagesPath, filename, themeName);
+            var virtualPath = ThemeVirtualPaths.Image(filename, themeName);
+            return urlHelper.Content(virtualPath);
         }
 
         /// <summary>
-        /// Application absolute path for a stylesheet file, taking themes into  consideration. Default theme name = 'Default'.
+        /// Application absolute path for a theme's stylesheet file.
         /// </summary>
         /// <param name="urlHelper">The URL helper.</param>
-        /// <param name="filename">The filename.</param>
+        /// <param name="filename">The filename for the stylesheet.</param>
         /// <returns></returns>
         [UsedImplicitly]
         public static string ThemeStylesheetUrl(this UrlHelper urlHelper, string filename)
@@ -44,36 +44,17 @@ namespace Quintsys.Web.ThemeableRazorViewEngine
         }
 
         /// <summary>
-        /// Application absolute path for a stylesheet file, taking themes into  consideration.
+        /// Application absolute path for a theme's stylesheet file.
         /// </summary>
         /// <param name="urlHelper">The URL helper.</param>
-        /// <param name="filename">The filename.</param>
+        /// <param name="filename">The filename for the stylesheet.</param>
         /// <param name="themeName">Name of the theme.</param>
         /// <returns></returns>
         [UsedImplicitly]
         public static string ThemeStylesheetUrl(this UrlHelper urlHelper, string filename, string themeName)
         {
-            return ContentUrl(urlHelper, ContentLocationFormats.StylesheetsPath, filename, themeName);
-        }
-
-        private static string ContentUrl(UrlHelper urlHelper,
-            [NotNull] string pathTemplate,
-            [NotNull] string filename,
-            string themeName = "Default")
-        {
-            if (pathTemplate == null)
-                throw new ArgumentNullException("pathTemplate");
-            if (filename == null)
-                throw new ArgumentNullException("filename");
-
-            var contentPath = string.Format(pathTemplate, themeName, filename);
-            return urlHelper.Content(contentPath);
-        }
-
-        private static class ContentLocationFormats
-        {
-            public const string ImagesPath =        "~/Themes/{0}/Content/images/{1}";
-            public const string StylesheetsPath =   "~/Themes/{0}/Content/css/{1}";
+            var virtualPath = ThemeVirtualPaths.Stylesheet(filename, themeName);
+            return urlHelper.Content(virtualPath);
         }
     }
 }
